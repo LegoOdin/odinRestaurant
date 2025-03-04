54683c23a41550ae6bacf8a9175181a281483f70
LegoOdin <96894245+LegoOdin@users.noreply.github.com>
2025-03-04 12:32:29 +1030

    menu working, styles being added

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 import homePage from "./home";
 import contactPage from "./contacts";
 import menuPage from "./menu";
+import logo from "./img/logoOne.png";
+import "./styles.css";
 
 const header = document.getElementById("header");
 const homeBtn = document.getElementById("home");
 const menuBtn = document.getElementById("menu");
 const contactBtn = document.getElementById("contact");
 
-
+const logoAndName = document.createElement("div");
+logoAndName.classList.add("banner")
 const shopName = document.createElement("h1");
 shopName.textContent = "Doggy Drop"
-header.appendChild(shopName);
+const logoImg = document.createElement("img");
+logoImg.src = logo;
+
+logoAndName.append(logoImg);
+logoAndName.append(shopName);
+
+header.prepend(logoAndName);
+
+
 
 
 
 homePage();
-
+homeBtn.disabled=true;
 
 homeBtn.addEventListener("click", ()=>{
     homePage();
+    homeBtn.disabled=true;
+    contactBtn.disabled=false;
+    menuBtn.disabled=false;
 })
 
 contactBtn.addEventListener("click", ()=>{
     contactPage();
+    homeBtn.disabled=false;
+    contactBtn.disabled=true;
+    menuBtn.disabled=false;
 })
 
 menuBtn.addEventListener("click", ()=>{
     menuPage();
+    homeBtn.disabled=false;
+    contactBtn.disabled=false;
+    menuBtn.disabled=true;
 })
--- src/menu.js
+++ src/menu.js
@@ -29,15 +29,12 @@ function menuPage() {
     }
 
     let hotDogMenu = new fullMenu();
-    hotDogMenu.newMenuItem("Beefy Boy", "", 19.99, false);
-    hotDogMenu.newMenuItem("Da Deutch", "", 19.99, false);
-    hotDogMenu.newMenuItem("Veggie Dog", "", 15.99, true);
-    hotDogMenu.newMenuItem("OG Dog", "", 12.99, false);
-    hotDogMenu.newMenuItem("American", "", 15.99, false);
-    hotDogMenu.newMenuItem("That's a Burger", "", 19.99, false);
-
-
-
+    hotDogMenu.newMenuItem("Beefy Boy", "Crafted with premium beef, this dog boasts a deep, smoky flavor and a satisfying snap. A timeless favorite.", 19.99, false);
+    hotDogMenu.newMenuItem("Der Deutsche", "Taste of Berlin. Spicy bratwurst, tangy kraut, and a mustard punch. Street food, elevated.", 19.99, false);
+    hotDogMenu.newMenuItem("Veggie Dog", "Flavor-packed, plant-based sausage, grilled to smoky perfection. Crafted with a blend of vegetables and spices, it delivers the classic hot dog experience, minus the meat.", 15.99, true);
+    hotDogMenu.newMenuItem("OG Dog", "Classic sausage, bun, and your favorite condiments. Pure and delicious.", 12.99, false);
+    hotDogMenu.newMenuItem("American", "American classic, this grilled sausage delivers a burst of savory flavor in every bite. A quick, delicious, and undeniably satisfying meal, perfect for any occasion.", 15.99, false);
+    hotDogMenu.newMenuItem("That's a Burger", "Satisfying blend of seasoned ground meat, a perfectly toasted bun, and a symphony of fresh toppings. A culinary staple that delivers comfort and flavor in every bite.", 19.99, false);
 
 
     hotDogMenu.menu.map(createMenuItem)
@@ -49,13 +46,23 @@ function menuPage() {
         
         
         const itemTitle = document.createElement("h3")
-        itemTitle.textContent = item;
-        
+
+        if(item.itemVeg){
+            itemTitle.textContent = item.itemName + " (V)";
+        } else {
+            itemTitle.textContent = item.itemName;
+        }
         itemContainer.appendChild(itemTitle);
 
+        const itemDescrition = document.createElement("p");
+        itemDescrition.textContent = item.itemDesc;
+        itemContainer.appendChild(itemDescrition);
+
+        const itemPrice = document.createElement("p");
+        itemPrice.textContent = "$" + item.itemPrice;
+        itemContainer.appendChild(itemPrice);
+
         content.appendChild(itemContainer);
-
-
     }
 
 }
